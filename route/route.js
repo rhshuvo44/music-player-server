bfc2f62cd7fcd74e6eb15bae686ea9f803711c69
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-01-17 13:33:09 +0600

    post api create

--- route/route.js
+++ route/route.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const upload = require("../config/multer");
 const audioModel = require("../modal/file");
-
+const mm = require("music-metadata");
 const route = express.Router();
 
+route.post("/uploadsong", upload.single("mp3file"), async (req, res) => {
+  const { path, originalname, filename } = req.file;
+  const metadata = await mm.parseFile(path);
+  const { title, artist, picture, album } = metadata.common;
 
-route.post("/uploadsong", upload.single("mp3file"));
+  res.json({
+    title: title ? title : filename.lastIndexOf(".mp3"),
+    artist: artist ? artist : "Unknown Artist",
+    album: album ? album : "Unknown album",
+    picture: picture ? picture[0].data.toString("base64") : null,
+  });
+
+  const audioSchema = new audioModel({
+    audioName: filename,
+    audioFileName: originalname,
+    audioPath: path,
+  });
+  await audioSchema.save();
+});
 module.exports = route;
