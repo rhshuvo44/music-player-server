f0600a348dca1ba0f000facc1e35002529f0b442
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-01-17 13:37:00 +0600

    get api create

--- route/route.js
+++ route/route.js
@@ -4,23 +4,33 @@ const audioModel = require("../modal/file");
 const mm = require("music-metadata");
 const route = express.Router();
 
-route.post("/uploadsong", upload.single("mp3file"), async (req, res) => {
-  const { path, originalname, filename } = req.file;
-  const metadata = await mm.parseFile(path);
-  const { title, artist, picture, album } = metadata.common;
+route
+  .post("/uploadsong", upload.single("mp3file"), async (req, res) => {
+    const { path, originalname, filename } = req.file;
+    const metadata = await mm.parseFile(path);
+    const { title, artist, picture, album } = metadata.common;
 
-  res.json({
-    title: title ? title : filename.lastIndexOf(".mp3"),
-    artist: artist ? artist : "Unknown Artist",
-    album: album ? album : "Unknown album",
-    picture: picture ? picture[0].data.toString("base64") : null,
+    res.json({
+      title: title ? title : filename.lastIndexOf(".mp3"),
+      artist: artist ? artist : "Unknown Artist",
+      album: album ? album : "Unknown album",
+      picture: picture ? picture[0].data.toString("base64") : null,
+    });
+
+    const audioSchema = new audioModel({
+      audioName: filename,
+      audioFileName: originalname,
+      audioPath: path,
+    });
+    await audioSchema.save();
+  })
+  .get("/", async (req, res) => {
+    try {
+      const songs = await audioModel.find();
+      res.json(songs);
+    } catch (error) {
+      console.log(error);
+    }
   });
 
-  const audioSchema = new audioModel({
-    audioName: filename,
-    audioFileName: originalname,
-    audioPath: path,
-  });
-  await audioSchema.save();
-});
 module.exports = route;
